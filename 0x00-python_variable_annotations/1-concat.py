e73bcda8d133fef3e6a7bca42884778ca464cd4e
rekebuba <abukiabdullahi1@gmail.com>
2024-08-01 14:10:03 +0300

    Basic annotations - concat

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+"""Basic annotations - concat """
+
+
+def concat(str1: str, str2: str) -> str:
+    """concatenate string
+
+    Args:
+        str1 (str):
+        str2 (str):
+
+    Returns:
+        str: concatenated string
+    """
+    return str1 + str2
