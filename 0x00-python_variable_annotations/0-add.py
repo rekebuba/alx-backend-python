4eec0c684c335232a34e288121b003e7b0346589
rekebuba <abukiabdullahi1@gmail.com>
2024-08-01 14:06:52 +0300

    0x00-python_variable_annotations

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+"""Basic annotations - add"""
+
+
+def add(a: float, b: float) -> float:
+    """
+
+    Args:
+        a (float)
+        b (float)
+
+    Returns:
+        float: sum
+    """
+    return a + b
