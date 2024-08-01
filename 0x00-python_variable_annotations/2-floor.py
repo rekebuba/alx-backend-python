978df286caec770b5037b7dd3ed1c43232ddc9c4
rekebuba <abukiabdullahi1@gmail.com>
2024-08-01 14:13:58 +0300

    Basic annotations - floor

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+"""Basic annotations - floor"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """_summary_
+
+    Args:
+        n (float)
+
+    Returns:
+        int: the floor of the float
+    """
+    return math.floor(n)
