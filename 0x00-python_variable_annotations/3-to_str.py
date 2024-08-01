536545bd863d920ba7b78ad1ec8a7529848cf76d
rekebuba <abukiabdullahi1@gmail.com>
2024-08-01 14:16:44 +0300

    Basic annotations - string

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+"""Basic annotations - to string"""
+
+
+def to_str(n: float) -> str:
+    """string representation of the float
+
+    Args:
+        n (float)
+
+    Returns:
+        str: string representation of the float
+    """
+
+    return str(float)
