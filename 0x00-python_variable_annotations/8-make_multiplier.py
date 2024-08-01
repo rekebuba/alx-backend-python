13674635bae1c4f1a35c64c87f9c950dd8922016
rekebuba <abukiabdullahi1@gmail.com>
2024-08-01 15:00:49 +0300

    Complex types - functions

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+"""Complex types - functions"""
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """takes a float multiplier as argument and returns a function
+
+    Args:
+        multiplier (float):
+
+    Returns:
+        Callable[[float], float]: function multiplies a float by multiplier.
+    """
+    return lambda x: x * multiplier
