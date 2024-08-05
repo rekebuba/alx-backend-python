b64fa01857d112e4e040a97f24f133a1baaf4c5a
rekebuba <abukiabdullahi1@gmail.com>
2024-08-05 21:19:11 +0300

    multiple coroutines at the same time with async

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python3
+"""multiple coroutines at the same time with async"""
+import asyncio
+import heapq
+from typing import List
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """multiple coroutines at the same time with async
+
+    Args:
+        n (int): spawn wait_random n times with the specified max_delay.
+        max_delay (int):
+
+    Returns:
+        List[float]: return the list of all the delays (float values).
+        The list of the delays should be in ascending order
+    """
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    delay = await asyncio.gather(*tasks)
+
+    return list(heapq.nsmallest(n, delay))
