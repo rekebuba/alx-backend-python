16250589a47596cf864ad78678765ba24c21d92b
rekebuba <abukiabdullahi1@gmail.com>
2024-08-05 21:06:48 +0300

    Creating Tasks

--- 0x01-python_async_function/3-main.py
+++ 0x01-python_async_function/3-main.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+import asyncio
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def test(max_delay: int):
+    task = task_wait_random(max_delay)
+    await task
+    print(task.__class__)
+
+asyncio.run(test(5))
--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+"""Creating Tasks"""
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """task_wait_random that takes an integer and returns a asyncio.Task.
+
+    Args:
+        max_delay (int):
+
+    Returns:
+        asyncio.Task: returns a asyncio.Task.
+    """
+    task = asyncio.create_task(wait_random(max_delay))
+    return task
