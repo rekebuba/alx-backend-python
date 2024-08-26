73ad75a9b9ec5076da47f08dbd566732133ba7b3
rekebuba <abukiabdullahi1@gmail.com>
2024-08-26 23:42:03 +0300

    0x03-Unittest_and_integration_tests

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -151,7 +151,6 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
         )
 
     def test_public_repos_with_license(self) -> None:
-        """Tests the `public_repos` method with a license."""
         self.assertEqual(
             GithubOrgClient("google").public_repos(license="apache-2.0"),
             self.apache2_repos,
